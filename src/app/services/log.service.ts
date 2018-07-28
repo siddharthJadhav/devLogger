3b4c8d0c76a350f370d1274aba7da1e9ad4d061b
Siddharth Jadhav <siddharth.jadhav2093@gmail.com>
2018-07-28 15:47:26 +0530

    implement local storage

--- src/app/services/log.service.ts
+++ src/app/services/log.service.ts
@@ -16,19 +16,20 @@ export class LogService {
   stateChange = this.stateSource.asObservable();
 
   constructor() {
-    this.logs = [{
-      id: '1', text: 'Genrate Combonents', date: new Date('12/27/2017 12:13:14')
-    }, {
-      id: '2', text: 'Add Bootstrap', date: new Date('12/27/2017 1:13:14')
-    }, {
-      id: '3', text: 'Genrate Logs', date: new Date('12/27/2017 2:13:14')
-    }];
+    // this.logs = [{
+    //   id: '1', text: 'Genrate Combonents', date: new Date('12/27/2017 12:13:14')
+    // }, {
+    //   id: '2', text: 'Add Bootstrap', date: new Date('12/27/2017 1:13:14')
+    // }, {
+    //   id: '3', text: 'Genrate Logs', date: new Date('12/27/2017 2:13:14')
+    // }];
 
     // this.logs = [];
 
    }
 
    getLogList() {
+    this.logs = localStorage.getItem('logs') === null ? [] : JSON.parse(localStorage.getItem('logs'));
      return of(this.logs);
    }
 
@@ -38,6 +39,7 @@ export class LogService {
 
    addLog(log: Log) {
       this.logs.unshift(log);
+    localStorage.setItem('logs', JSON.stringify(this.logs));
    }
 
    updatedLog(log: Log) {
@@ -47,6 +49,7 @@ export class LogService {
       }
     });
     this.logs.unshift(log);
+    localStorage.setItem('logs', JSON.stringify(this.logs));
    }
 
    deleteLog(log: Log) {
@@ -55,6 +58,7 @@ export class LogService {
         this.logs.splice(index, 1);
       }
     });
+    localStorage.setItem('logs', JSON.stringify(this.logs));
    }
 
    changeState() {
